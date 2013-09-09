7d710394cc213ebb639969a223970e1629fce67c
Maxime Jeanmart <maxime.jeanmart@gmail.com>
2013-09-09 21:34:12 +0200

    change the console.sh script to work in the installation folder

--- src/main/resources/scripts/console.sh
+++ src/main/resources/scripts/console.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-java -jar roq-swing-console-1.0-SNAPSHOT-jar-with-dependencies.jar
+echo "Starting Global configuration process"
+#Get the local directory in order to set the full path to the log4j config file
+ROQ_BIN=$(dirname "$(readlink -f "$(type -P $0 || echo $0)")")
+ROQ_HOME=$(dirname $ROQ_BIN)
+echo "ROQ BIN Home= $ROQ_HOME"
+java -jar ../lib/roq-swing-console-1.0-SNAPSHOT-jar-with-dependencies.jar
 
